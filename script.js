4bbd6479297b610d989a64cb583b3b5c356a6eb0
ilyasonar <132559146+ilyasonar@users.noreply.github.com>
2024-11-20 00:04:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -15,35 +15,37 @@ navbarLinks.forEach((link) => {
   link.addEventListener("click", () => menuOpenButton.click());
 });
 
-/* Initializing Swiper */
-let swiper = new Swiper(".slider-wrapper", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 25,
+/* Initializing Swiper only if the slider exists */
+if (document.querySelector(".slider-wrapper")) {
+  let swiper = new Swiper(".slider-wrapper", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
 
-  // Pagination bullets
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
+    // Pagination bullets
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  /* Responsive breakpoints */
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+    /* Responsive breakpoints */
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
     },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-  },
-});
+  });
+}
